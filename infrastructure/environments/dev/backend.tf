b25ed3240459a915e89eb315e0c5203ec03729a0
Ousmane Sanokho <ousmanesanokho74@email.com>
2026-08-22 17:27:31 +0800

    Fix backend.tf: actually apply use_lockfile change

--- infrastructure/environments/dev/backend.tf
+++ infrastructure/environments/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "deployguard-tfstate-ousmane-2989"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "deployguard-tfstate-lock"
-    encrypt        = true
+    bucket       = "deployguard-tfstate-ousmane-2989"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
