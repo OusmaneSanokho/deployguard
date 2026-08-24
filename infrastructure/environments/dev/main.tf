ec6fb56694381c8794af7735e68cb4a29e00e81d
Ousmane Sanokho <ousmanesanokho74@email.com>
2026-08-25 05:34:20 +0800

    Add GitHub OIDC provider and CI role scoped to this repo

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -360,3 +360,44 @@ resource "aws_iam_role" "ecs_task" {
     Name = "deployguard-ecs-task-role"
   }
 }
+data "tls_certificate" "github_oidc" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.github_oidc.certificates[0].sha1_fingerprint]
+
+  tags = {
+    Name = "deployguard-github-oidc"
+  }
+}
+resource "aws_iam_role" "github_actions" {
+  name = "deployguard-github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:OusmaneSanokho/deploguard:*"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "deployguard-github-actions-role"
+  }
+}
